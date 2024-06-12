904197c5980783b2cc395f49f46b109c590e9ca1
hoangtan282 <tan.nguyen@sipher.xyz>
2024-06-12 11:10:49 +0700

    remove manual fix

--- script/constants/projectConstants/supertoken/sipher_token_testnet.ts
+++ script/constants/projectConstants/supertoken/sipher_token_testnet.ts
@@ -1,48 +1,50 @@
-
 import {
-    ChainSlug,
-    DeploymentMode,
-    IntegrationTypes,
+  ChainSlug,
+  DeploymentMode,
+  IntegrationTypes,
 } from "@socket.tech/dl-core";
 import { Hooks, ProjectConstants } from "../../../../src";
 import { Tokens } from "../../../../src/enums";
 
 export const pc: ProjectConstants = {
-    [DeploymentMode.PROD]: {
-  [Tokens.SIPHER]: {
-    vaultChains: [ChainSlug.SEPOLIA],
-    controllerChains: [ChainSlug.OPTIMISM_SEPOLIA, ChainSlug.SIPHER_FUNKI_TESTNET],
-    hook: {
-      hookType: Hooks.LIMIT_HOOK,
-      limitsAndPoolId: {
-        [ChainSlug.SIPHER_FUNKI_TESTNET]: {
-          [IntegrationTypes.fast]: {
-            sendingLimit: "10000",
-            receivingLimit: "10000"
-          }
+  [DeploymentMode.PROD]: {
+    [Tokens.SIPHER]: {
+      vaultChains: [ChainSlug.SEPOLIA],
+      controllerChains: [
+        ChainSlug.OPTIMISM_SEPOLIA,
+        ChainSlug.SIPHER_FUNKI_TESTNET,
+      ],
+      hook: {
+        hookType: Hooks.LIMIT_HOOK,
+        limitsAndPoolId: {
+          [ChainSlug.SIPHER_FUNKI_TESTNET]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "10000",
+              receivingLimit: "10000",
+            },
+          },
+          [ChainSlug.SEPOLIA]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "10000",
+              receivingLimit: "10000",
+            },
+          },
+          [ChainSlug.OPTIMISM_SEPOLIA]: {
+            [IntegrationTypes.fast]: {
+              sendingLimit: "10000",
+              receivingLimit: "10000",
+            },
+          },
         },
-        [ChainSlug.SEPOLIA]: {
-          [IntegrationTypes.fast]: {
-            sendingLimit: "10000",
-            receivingLimit: "10000"
-          }
-        },
-        [ChainSlug.OPTIMISM_SEPOLIA]: {
-          [IntegrationTypes.fast]: {
-            sendingLimit: "10000",
-            receivingLimit: "10000"
-          }
-        }
-      }
+      },
+      superTokenInfo: {
+        name: "Sipher Token",
+        symbol: Tokens.SIPHER,
+        decimals: 18,
+        owner: "0xD6824e69Bc24889740fDfC6371Bf985A7ab7ac7f",
+        initialSupplyOwner: "0xD6824e69Bc24889740fDfC6371Bf985A7ab7ac7f",
+        initialSupply: "0",
+      },
     },
-    superTokenInfo: {
-      name: "Sipher Token",
-      symbol: Tokens.SIPHER,
-      decimals: 18,
-      owner: "0xD6824e69Bc24889740fDfC6371Bf985A7ab7ac7f",
-      initialSupplyOwner: "0xD6824e69Bc24889740fDfC6371Bf985A7ab7ac7f",
-      initialSupply: "0"
-    }
-  }
-}
+  },
 };
